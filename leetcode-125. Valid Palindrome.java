1ab7c797516845ed1fc444b44b8a8ac76f428656
Saranyanandhini <113166835+Saranyanandhini@users.noreply.github.com>
2022-10-19 15:03:27 +0530

    Add files via upload

---
+++ leetcode-125. Valid Palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i)>=48 && s.charAt(i)<=57 || s.charAt(i)>=97 && s.charAt(i)<=122){
+			// if ch is in between [0-9 or a-z]
+                str.append(s.charAt(i));
+            }
+        }
+        return isPalin(str);
+    }
+    
+     static boolean isPalin(StringBuilder str){
+        int start=0;
+        int end=str.length()-1;
+        while (start<end){
+            if (str.charAt(start)!=str.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
